e3cfd20e19d549520106edbaa820038b63c51162
depippo <jdepippo@gmail.com>
2017-04-01 06:47:25 -0400

    Fleshed out search controller to use search service

--- app/assets/javascripts/controllers/SearchCtrl.js
+++ app/assets/javascripts/controllers/SearchCtrl.js
@@ -1,5 +1,36 @@
+function SearchController($scope, $http, SearchService) {
+
+  var ctrl = this;
+
+  ctrl.venueSearch = function(city) {
+    SearchService.getVenues(city)
+    .done(function(response) {
+      $scope.searchResults = [];
+      response.response.venues.forEach(function(venue){
+        $scope.searchResults.push(venue);
+      });
+      console.log($scope.searchResults);
+      $scope.searchResults.forEach(function(item){
+        console.log(item.name);
+      })
+      SearchService.searchResults = $scope.searchResults;
+    })
+  }
+
+  ctrl.mallSearch = function(ll) {
+    SearchService.searchWithin(ll)
+    .done(function(response) {
+      $scope.veganResults = [];
+      response.response.groups[0].items.forEach(function(item){
+        $scope.veganResults.push(item);
+      });
+      SearchService.veganResults = $scope.veganResults;
+      console.log($scope.veganResults);
+    })
+  }
+
+}
+
 angular
   .module('vegfinder')
-  .controller('SearchCtrl', function($scope, $rootScope, Auth){
-    $scope.search = "Welcome to the search page"
-  })
+  .controller('SearchController', SearchController);
