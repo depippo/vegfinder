f6082e9bc54c86b5c9f32393a585bce112c90337
depippo <jdepippo@gmail.com>
2017-04-07 18:26:12 -0400

    Places view page has a search box to filter restaurants by location

--- app/assets/javascripts/controllers/PlacesCtrl.js
+++ app/assets/javascripts/controllers/PlacesCtrl.js
@@ -1,9 +1,17 @@
-function PlacesController($scope, places, SearchService, PlaceService, $rootScope) {
+function PlacesController($scope, places, SearchService, PlaceService, $rootScope, $filter) {
    $scope.places = [];
    
    places.data.forEach(function(place){
     $scope.places.push(place);
    })
+
+  this.search = '';
+
+  this.refilter = function() {
+    this.filteredList = $filter('filter')($scope.places, this.search);
+  };
+
+  this.refilter();
 }
 
 angular
