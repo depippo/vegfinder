e51046d5b1f884c36e10a9f1b6fe64a7d999ceb0
depippo <jdepippo@gmail.com>
2017-04-10 11:44:42 -0400

    Reworked details view to show several more fields from API

--- app/assets/javascripts/controllers/DetailsCtrl.js
+++ app/assets/javascripts/controllers/DetailsCtrl.js
@@ -1,11 +1,55 @@
 function DetailsController($scope, details, SearchService, CollectionService) {
 
   $scope.details = details.response;
+  $scope.price = "";
+  $scope.reservations = "";
+  $scope.creditcards = "";
+  $scope.outdoor = "";
+  $scope.menus = [];
+  $scope.drinks = [];
+  $scope.options = [];
+
 
   var ctrl = this;
 
-  ctrl.showPlaceInfo = function() {
+  ctrl.getPlaceInfo = function() {
     console.log($scope.details);
+    $scope.details.venue.attributes.groups.forEach(function(group){
+      if(group.name == "Price"){
+        $scope.price = group.items[0].displayValue;
+        console.log($scope.price);
+      }
+      if(group.name == "Reservations"){
+        $scope.reservations = group.items[0].displayValue;
+        console.log($scope.reservations);
+      }
+      if(group.name == "Credit Cards"){
+        $scope.creditcards = group.items[0].displayValue;
+        console.log($scope.creditcards);
+      }
+      if(group.name == "Outdoor Seating"){
+        $scope.outdoor = group.items[0].displayValue;
+        console.log($scope.outdoor);
+      }
+      if(group.name == "Menus"){
+        group.items.forEach(function(item){
+          $scope.menus.push(item.displayName);
+        });
+        console.log($scope.menus);
+      }
+      if(group.name == "Drinks"){
+        group.items.forEach(function(item){
+          $scope.drinks.push(item.displayName);
+        });
+        console.log($scope.drinks);
+      }
+      if(group.name == "Dining Options"){
+        group.items.forEach(function(item){
+          $scope.options.push(item.displayName);
+        });
+        console.log($scope.options);
+      }
+    })
   }
 
   ctrl.saveToCollection = function(details, id){
