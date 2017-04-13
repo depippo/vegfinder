e34521f562f8850139e5317d9b9835a5438c776e
depippo <jdepippo@gmail.com>
2017-04-13 08:18:48 -0400

    Removed UpdateService, re-formatted empty user collection message.

--- app/assets/javascripts/controllers/AuthCtrl.js
+++ app/assets/javascripts/controllers/AuthCtrl.js
@@ -1,11 +1,10 @@
 angular
   .module('vegfinder')
-  .controller('AuthCtrl', function($scope, $rootScope, Auth, UpdateService, $state){
+  .controller('AuthCtrl', function($scope, $rootScope, Auth, $state){
     var config = {headers: {'X-HTTP-Method-Override': 'POST'}}
 
     $scope.register = function(){
       Auth.register($scope.user, config).then(function(user){
-        UpdateService.updateLists(user)
         $rootScope.user = user
         alert("Thanks for signing up, " + user.username);
         $state.go('home');
@@ -16,7 +15,6 @@ angular
 
     $scope.login = function(){
       Auth.login($scope.user, config).then(function(user){
-        UpdateService.updateLists(user)
         $rootScope.user = user
         alert("You're signed in, " + user.username);
         $state.go('home');
